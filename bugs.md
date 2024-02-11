5603b1b9ee80af53d2997b08359da9e769ea05dc
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-02-11 14:06:45 +0530

    init

---
+++ bugs.md
@@ -0,0 +1,1 @@
+# Put bugs Here
---
+++ frontend/master/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        "SAPBlue-100": "#bcc4de",
+        "SAPBlue-300": "#8c9ecd",
+        "SAPBlue-500": "#6f89c3",
+        "SAPBlue-700": "#5076ba",
+        "SAPBlue-800": "#3f6eb6",
+        "SAPBlue-900": "#0070f2",
+      }
+    },
+  },
+  plugins: [],
+}
+
