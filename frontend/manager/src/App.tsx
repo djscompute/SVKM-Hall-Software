ee7c5fefd3cb63086128c1729cfa0725ab5087e3
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2024-02-11 14:21:57 +0530

    everything setup

--- frontend/manager/src/App.tsx
+++ frontend/manager/src/App.tsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col ">
+      <div className=" w-full h-40 bg-SAPBlue-100">a</div>
+      <div className=" w-full h-40 bg-SAPBlue-300">a</div>
+      <div className=" w-full h-40 bg-SAPBlue-500">a</div>
+      <div className=" w-full h-40 bg-SAPBlue-700">a</div>
+      <div className=" w-full h-40 bg-SAPBlue-800">a</div>
+      <div className=" w-full h-40 bg-SAPBlue-900">a</div>
+    </div>
+  );
 }
