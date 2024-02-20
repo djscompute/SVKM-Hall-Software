ac5568c85cd73707e0b778176a5c7f6bd39aee49
Maaz Saboowala <maazsaboowala07@gmail.com>
2024-02-21 00:16:04 +0530

    Added Input fields performing CRUD operation in About_Hall & Hall_Additional_Features

--- frontend/master/src/components/hallInfo/AboutHall.tsx
+++ frontend/master/src/components/hallInfo/AboutHall.tsx
@@ -3,34 +3,62 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import { EachHallType } from "../../types/Hall.types";
 
-type props = {
+type Props = {
   about: string[];
   setHallData: React.Dispatch<React.SetStateAction<EachHallType>>;
 };
 
-export default function AboutHall({ about, setHallData }: props) {
+export default function AboutHall({ about, setHallData }: Props) {
   const [modalData, setModalData] = useState<string[]>(about);
   const [isOpen, setIsOpen] = useState(false);
   const [modal, setModal] = useState(false);
+  const [aboutList, setAboutList] = useState<string[]>(modalData);
+  const [newItem, setNewItem] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
 
   const toggleReadMore = () => setIsOpen(!isOpen);
 
-  const toggleModal = () => setModal(!modal);
-
-  const handleChange = (event: React.ChangeEvent<any>) => {
-    const { name, value } = event.target;
-    setModalData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  const toggleModal = () => {
+    setAboutList(modalData);
+    setModal(!modal);
   };
 
-  const updateHallAbout = () => {
+  const handleFormSubmit = () => {
+    setModalData(aboutList);
     setHallData((prev) => ({
       ...prev,
-      about: modalData,
+      about: aboutList,
     }));
     toggleModal();
+    setEditIndex(-1); 
+  };
+
+  const handleAddItem = () => {
+    if (newItem.trim() !== "") {
+      if (editIndex !== -1) {
+        
+        const updatedList = [...aboutList];
+        updatedList[editIndex] = newItem;
+        setAboutList(updatedList);
+        setEditIndex(-1);
+      } else {
+        
+        setAboutList([...aboutList, newItem]);
+      }
+      setNewItem("");
+    }
+  };
+
+  const handleEditItem = (index: number) => {
+    setNewItem(aboutList[index]);
+    setEditIndex(index);
+  };
+
+  const handleDeleteItem = (index: number) => {
+    const updatedList = [...aboutList];
+    updatedList.splice(index, 1);
+    setAboutList(updatedList);
+    setEditIndex(-1); 
   };
 
   return (
@@ -41,7 +69,6 @@ export default function AboutHall({ about, setHallData }: props) {
           className={`about-hall text-lg ${
             isOpen ? "" : " h-24 overflow-hidden"
           }`}
-          //   style={isOpen ? null : paragraphStyles}
         >
           {about.map((eachPara, index) => (
             <p key={index} className="my-2">
@@ -66,22 +93,41 @@ export default function AboutHall({ about, setHallData }: props) {
         )}
       </div>
       <div className="hall-info-edit h-fit relative">
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          className="show-on-hover h-6 cursor-pointer opacity-50 hover:opacity-100"
-          onClick={toggleModal}
-        />
+        <div className="show-on-hover cursor-pointer opacity-100 hover:opacity-100">
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            className="show-on-hover h-6 cursor-pointer opacity-50 hover:opacity-100"
+            onClick={toggleModal}
+          />
+        </div>
         {modal && (
           <div className="modal-message fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
             <div className="message bg-white p-6 rounded w-3/5">
-              <div className="flex gap-3 mb-5">
-                <h1 className="w-20 font-semibold">About:</h1>
-                <div className="bg-black text-white px-5 py-2 rounded w-full">
-                  {about.map((eachPara, index) => (
-                    <p key={index} className="mb-2">
-                      {eachPara}
-                    </p>
+              <div className="flex flex-col gap-3 mb-5">
+                <label htmlFor="info" className="font-semibold">
+                  About:
+                </label>
+                <ul>
+                  {aboutList.map((item, index) => (
+                    <li key={index} className="flex items-center justify-between">
+                      <span className="p-4">{item}</span>
+                      <div className="flex gap-2">
+                        <button className="bg-blue-700 p-2 rounded text-white hover:bg-blue-500 transform active:scale-95 transition duration-300" onClick={() => handleEditItem(index)}>Edit</button>
+                        <button className="bg-red-700 p-2 rounded text-white hover:bg-red-500 transform active:scale-95 transition duration-300" onClick={() => handleDeleteItem(index)}>Delete</button>
+                      </div>
+                    </li>
                   ))}
+                </ul>
+                <div className="flex gap-2">
+                  <input
+                    type="text"
+                    value={newItem}
+                    onChange={(e) => setNewItem(e.target.value)}
+                    className="bg-gray-300 text-black px-5 py-2 rounded resize-none flex-grow border-x-black"
+                  />
+                  <button className="bg-green-700 p-2 rounded text-white hover:bg-green-500 transform active:scale-95 transition duration-300" onClick={handleAddItem}>
+                    {editIndex !== -1 ? 'Update' : 'Add'}
+                  </button>
                 </div>
               </div>
               <div className="buttons flex justify-end gap-3 mt-5">
@@ -91,7 +137,10 @@ export default function AboutHall({ about, setHallData }: props) {
                 >
                   Cancel
                 </button>
-                <button className="bg-SAPBlue-700 p-2 rounded text-white hover:bg-SAPBlue-900 transform active:scale-95 transition duration-300">
+                <button
+                  className="bg-SAPBlue-700 p-2 rounded text-white hover:bg-SAPBlue-900 transform active:scale-95 transition duration-300"
+                  onClick={handleFormSubmit}
+                >
                   Submit
                 </button>
               </div>
--- frontend/master/src/components/hallInfo/HallAdditionalFeatures.tsx
+++ frontend/master/src/components/hallInfo/HallAdditionalFeatures.tsx
@@ -1,64 +1,140 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
-import {
-  EachHallAdditonalFeaturesType,
-  EachHallType,
-} from "../../types/Hall.types";
+import { EachHallAdditonalFeaturesType, EachHallType } from "../../types/Hall.types";
 
-type props = {
+type Props = {
   additionalFeatures: EachHallAdditonalFeaturesType[];
   setHallData: React.Dispatch<React.SetStateAction<EachHallType>>;
 };
 
-const HallAdditionalFeatures = ({ additionalFeatures, setHallData }: props) => {
-
+const HallAdditionalFeatures = ({ additionalFeatures, setHallData }: Props) => {
+  const [modalData, setModalData] = useState<EachHallAdditonalFeaturesType[]>(additionalFeatures);
   const [modal, setModal] = useState(false);
+  const [editedFeatures, setEditedFeatures] = useState<EachHallAdditonalFeaturesType[]>(modalData);
+  const [newItem, setNewItem] = useState<EachHallAdditonalFeaturesType>({ heading: "", desc: "" });
+  const [editIndex, setEditIndex] = useState<number>(-1);
 
-  const toggleModal = () => setModal(!modal);
+  const toggleModal = () => {
+    setEditedFeatures(modalData);
+    setModal(!modal);
+  };
+
+  const handleFormSubmit = () => {
+    setModalData(editedFeatures);
+    setHallData((prev) => ({
+      ...prev,
+      additionalFeatures: editedFeatures,
+    }));
+    toggleModal();
+    setEditIndex(-1);
+  };
+
+  const handleAddItem = () => {
+    if (newItem.heading.trim() !== "" && newItem.desc.trim() !== "") {
+      if (editIndex !== -1) {
+        const updatedList = [...editedFeatures];
+        updatedList[editIndex] = newItem;
+        setEditedFeatures(updatedList);
+        setEditIndex(-1);
+      } else {
+        setEditedFeatures([...editedFeatures, newItem]);
+      }
+      setNewItem({ heading: "", desc: "" });
+    }
+  };
+
+  const handleEditItem = (index: number) => {
+    setNewItem(editedFeatures[index]);
+    setEditIndex(index);
+  };
+
+  const handleDeleteItem = (index: number) => {
+    const updatedList = [...editedFeatures];
+    updatedList.splice(index, 1);
+    setEditedFeatures(updatedList);
+    setEditIndex(-1);
+  };
 
   return (
-    <div className="hall-additional-features flex justify-between bg-blue-100 w-full py-5 px-7 rounded-lg">
-      <div className="hall-additional-features-info">
-        <h2 className="additional-features-text font-bold text-xl mb-3">
-          Additional Features
-        </h2>
-        {additionalFeatures.map((eachFeature) => (
-          <div className="flex flex-col">
-            <p className=" font-medium text-lg">{eachFeature.heading}</p>
-            <p>{eachFeature.desc}</p>
-          </div>
-        ))}
+    <div className="about-hall flex justify-between bg-blue-100 w-full py-5 px-7 rounded-lg">
+      <div className="hall-additional-features-info w-11/12">
+        <h2 className="font-bold text-xl mb-3">Additional Features</h2>
+        <div className="about-hall text-lg">
+          {additionalFeatures.map((feature, index) => (
+            <div key={index} className="flex flex-col mb-3">
+              <p className="font-medium text-lg">{feature.heading}</p>
+              <p>{feature.desc}</p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="hall-additional-features h-fit relative">
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          className="show-on-hover h-6 cursor-pointer opacity-50 hover:opacity-100"
-          onClick={toggleModal}
-        />
-        {modal &&
-          (<div className="modal-message fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
+      <div className="hall-info-edit h-fit relative">
+      <div className="show-on-hover cursor-pointer opacity-100 hover:opacity-100">
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            className="show-on-hover h-6 cursor-pointer opacity-50 hover:opacity-100"
+            onClick={toggleModal}
+          />
+        </div>
+        
+        {modal && (
+          <div className="modal-message fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
             <div className="message bg-white p-6 rounded w-3/5">
-              <div className="flex gap-3 mb-5">
-                <h1 className="w-20 font-semibold">Additional Features:</h1>
-                <div className="bg-black text-white px-5 py-2 rounded w-full ">
-                  { additionalFeatures.map((eachFeature) => (
-                    <div className="flex flex-col">
-                      <p className="each-feature-heading font-medium text-lg">{eachFeature.heading}</p>
-                      <p className="each-feature-desc">{eachFeature.desc}</p>
-                    </div>
+              <div className="flex flex-col gap-3 mb-5">
+                <label htmlFor="info" className="font-semibold">
+                  Additional Features:
+                </label>
+                <ul>
+                  {editedFeatures.map((item, index) => (
+                    <li key={index} className="flex items-center justify-between">
+                      <span className="p-4">{item.heading}</span>
+                      <div className="flex gap-2">
+                        <button className="bg-blue-700 p-2 rounded text-white hover:bg-blue-500 transform active:scale-95 transition duration-300" onClick={() => handleEditItem(index)}>Edit</button>
+                        <button className="bg-red-700 p-2 rounded text-white hover:bg-red-500 transform active:scale-95 transition duration-300" onClick={() => handleDeleteItem(index)}>Delete</button>
+                      </div>
+                    </li>
                   ))}
+                </ul>
+                <div className="flex gap-2">
+                  <input
+                    type="text"
+                    value={newItem.heading}
+                    onChange={(e) => setNewItem({ ...newItem, heading: e.target.value })}
+                    className="bg-gray-300 text-black px-5 py-2 rounded resize-none flex-grow border-x-black"
+                    placeholder="Heading"
+                  />
+                  <textarea
+                    value={newItem.desc}
+                    onChange={(e) => setNewItem({ ...newItem, desc: e.target.value })}
+                    className="bg-gray-300 text-black px-5 py-2 rounded resize-none flex-grow border-x-black"
+                    placeholder="Description"
+                  />
+                  <button className="bg-green-700 p-2 rounded text-white hover:bg-green-500 transform active:scale-95 transition duration-300" onClick={handleAddItem}>
+                    {editIndex !== -1 ? 'Update' : 'Add'}
+                  </button>
                 </div>
               </div>
               <div className="buttons flex justify-end gap-3 mt-5">
-                <button className="bg-red-700 p-2 rounded text-white hover:bg-red-500 transform active:scale-95 transition duration-300" onClick={toggleModal}>Cancel</button>
-                <button className="bg-SAPBlue-700 p-2 rounded text-white hover:bg-SAPBlue-900 transform active:scale-95 transition duration-300">Submit</button>
+                <button
+                  className="bg-red-700 p-2 rounded text-white hover:bg-red-500 transform active:scale-95 transition duration-300"
+                  onClick={toggleModal}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="bg-SAPBlue-700 p-2 rounded text-white hover:bg-SAPBlue-900 transform active:scale-95 transition duration-300"
+                  onClick={handleFormSubmit}
+                >
+                  Submit
+                </button>
               </div>
             </div>
-          </div>)
-        }
+          </div>
+        )}
       </div>
     </div>
   );
 };
+
 export default HallAdditionalFeatures;
